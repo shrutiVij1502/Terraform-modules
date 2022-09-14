de1bc9c2d9692dacac2f999dc7cb620f1e6c86be
Shruti Vij <67600604+shrutiVij1502@users.noreply.github.com>
2022-09-14 10:51:15 +0530

    Update ec2.tf

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -1,1 +1,24 @@
+# Variables
+variable "image" {}
+variable "instance_type" {}
+variable "key_pair" {}
+variable "subnetID" {
+  type = string
+}
 
+# EC2 Instance
+resource "aws_instance" "nginx_server" {
+  ami           = var.image
+  instance_type = var.instance_type
+  key_name = var.key_pair
+  subnet_id = var.subnetID
+
+tags = {
+    Name = "tf-test"
+  }
+
+}
+
+output "instanceID" {
+  value = aws_instance.nginx_server.id
+}
